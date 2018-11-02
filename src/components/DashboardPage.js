27232ce7ad04e9dbe96fcee54dd946f3bfea988d
Alper Donmez <alper.donmez490@gmail.com>
2018-11-02 14:54:53 -0400

    initial commit

---
+++ src/components/DashboardPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DashboardPage extends React.Component {
+  render() {
+    return (
+      <div className="dashboard-page">
+        Dashboard page content
+      </div>
+    );
+  }
+}
+
+export default DashboardPage;
